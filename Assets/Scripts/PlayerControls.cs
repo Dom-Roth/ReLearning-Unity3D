b7d33cd742a65b3af2ac67bcae6f0b0d4531250d
Dominik Roth <dominik@roth.systems>
2021-04-15 03:42:33 +0200

    Tutorial at 1:52.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     public float moveSpeed = 1.5f;
+    public GameObject playerDragon;
 
     // Update is called once per frame
     void Update()
@@ -12,18 +13,39 @@ public class PlayerControls : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+            playerDragon.transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+            playerDragon.transform.localRotation = Quaternion.Euler(0, 270, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+            playerDragon.transform.localRotation = Quaternion.Euler(0, 180, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+            playerDragon.transform.localRotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            playerDragon.transform.localRotation = Quaternion.Euler(0, 315, 0);
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            playerDragon.transform.localRotation = Quaternion.Euler(0, 45, 0);
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            playerDragon.transform.localRotation = Quaternion.Euler(0, 225, 0);
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            playerDragon.transform.localRotation = Quaternion.Euler(0, 135, 0);
         }
     }
 }
